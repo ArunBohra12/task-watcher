1cba96214c045fd0c34acfbccae12ade13ab57c1
Arun Bohra <arunbohra12@gmail.com>
2022-12-12 11:21:52 +0530

    Add dotenv package and db url to config.env

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+/* This is the entry point of the application.
+  It is the first file that is executed when the application starts.
+*/
+
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+dotenv.config({ path: "./config.env" });
+
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/task-watcher");
+mongoose.connect(process.env.DATABASE_URL);
 
 const app = require("./app");
 
